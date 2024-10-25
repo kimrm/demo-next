5ad41301a13fb89834ac1aef52b73e2153121754
Kim Rune Møller <kimrune@hotmail.no>
2024-10-25 15:19:56 +0200

    test server action

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,42 @@
 import React from "react";
 
-export default function Test() {
-  return <div>page</div>;
+async function fetchData() {
+  "use server";
+  const data = [
+    {
+      name: "John Doe",
+      age: 29
+    },
+    {
+      name: "Jane Doe",
+      age: 32
+    },
+    {
+      name: "John Smith",
+      age: 41
+    },
+    {
+      name: "Jane Smith",
+      age: 36
+    }
+  ];
+
+  return data;
+}
+
+export default async function Test() {
+  const data = await fetchData();
+  return (
+    <div>
+      <h1>Data</h1>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <h2>{item.name}</h2>
+            <p>Age: {item.age}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
